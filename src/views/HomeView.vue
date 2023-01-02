2bfdf7f7b632d431579c03182a8d5c9beba77c26
Diego Lirio <ddamacena@uolinc.com>
2023-01-02 12:31:19 -0300

    initing-product-page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -44,33 +44,33 @@ export default defineComponent({
             :key="n"
             xl="6" lg="4" md="4" sm="4" xs="6" cols="12" 
           >
-            
-            <v-card>
-              <v-img
-                :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-                aspect-ratio="1"
-                cover
-                class="bg-grey-lighten-2"
-              >
-                <template v-slot:placeholder>
-                  <v-row
-                    class="fill-height ma-0"
-                    align="center"
-                    justify="center"
+            <router-link to="/p/1234567890" style="color: inherit; text-decoration: none">
+                <v-card>
+                  <v-img
+                    :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                    :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                    aspect-ratio="1"
+                    cover
+                    class="bg-grey-lighten-2"
                   >
-                    <v-progress-circular
-                      indeterminate
-                      color="grey-lighten-5"
-                    ></v-progress-circular>
-                  </v-row>
-                </template>
-              </v-img>
-            </v-card>
-            <p v-if="n < 5">Newmind Lâmpada, lâmpadas sala de, luminárias decorativas e refletor de, lâmpadas de decoração de de hotel, Preto 24x20x12,5cm </p>
-            <p v-else>Newmind Lâmpada, lâmpadas sala  </p>
-            <b>R$ 189,00</b>              
-              
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular
+                          indeterminate
+                          color="grey-lighten-5"
+                        ></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card>
+                <p v-if="n < 5">Newmind Lâmpada, lâmpadas sala de, luminárias decorativas e refletor de, lâmpadas de decoração de de hotel, Preto 24x20x12,5cm </p>
+                <p v-else>Newmind Lâmpada, lâmpadas sala  </p>
+                <b>R$ 189,00</b>              
+              </router-link>  
           </v-col>
 
 
---
+++ src/views/ProductView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Product">
+    <h1>This is an product page</h1>
+  </div>
+</template>
