58ea845ea3e8c9f1e4bbd0b9795f6aa9c7366b20
Diego Lirio <ddamacena@uolinc.com>
2023-01-02 12:09:32 -0300

    Template

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -42,10 +42,39 @@ export default defineComponent({
           <v-col
             v-for="n in 24"
             :key="n"
-            cols="4"
+            xl="6" lg="4" md="4" sm="4" xs="6" cols="12" 
           >
-            <v-card height="200"></v-card>
+            
+            <v-card>
+              <v-img
+                :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                aspect-ratio="1"
+                cover
+                class="bg-grey-lighten-2"
+              >
+                <template v-slot:placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                  >
+                    <v-progress-circular
+                      indeterminate
+                      color="grey-lighten-5"
+                    ></v-progress-circular>
+                  </v-row>
+                </template>
+              </v-img>
+            </v-card>
+            <p v-if="n < 5">Newmind Lâmpada, lâmpadas sala de, luminárias decorativas e refletor de, lâmpadas de decoração de de hotel, Preto 24x20x12,5cm </p>
+            <p v-else>Newmind Lâmpada, lâmpadas sala  </p>
+            <b>R$ 189,00</b>              
+              
           </v-col>
+
+
+          <!-- https://raw.githubusercontent.com/LaCasaDeLustre/LaCasaDeLustre.github.io/master/lustres/IMG-7928.jpg -->
         </v-row>
       </v-container>
     </v-main>
