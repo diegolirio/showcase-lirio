fbe3449232b77dd31baf5c33952713cd2fe862e3
Diego Lirio <ddamacena@uolinc.com>
2023-01-02 15:44:23 -0300

    Button WhatsApp

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -8,13 +8,13 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>
-        <router-link to="/" style="color: inherit; text-decoration: none">
+        <!-- <router-link to="/" style="color: inherit; text-decoration: none"> -->
               <img 
                 src="https://raw.githubusercontent.com/LaCasaDeLustre/LaCasaDeLustre.github.io/master/oshine/images/logo-lacasadelustre-Page-1-transparente.png" 
                 height="60"
                 style="padding-top: 7px"
               />
-        </router-link>
+        <!-- </router-link> -->
       </v-toolbar-title>
     </v-app-bar>
 </template>
