49811665815bb15c3f36680e60fd713fe16ca2af
Diego Lirio <ddamacena@uolinc.com>
2023-01-05 23:51:02 -0300

    +

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
+  },
+  page: {
+    entry: 'src/main.js',
+    title: 'Minha Loja',    
   }
 })
