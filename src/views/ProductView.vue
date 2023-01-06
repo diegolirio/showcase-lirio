b7d19d6aa91492ab54e800dcd069a8b30af400b4
Diego Lirio <ddamacena@uolinc.com>
2023-01-06 18:05:47 -0300

    Carousel Vue

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -9,30 +9,40 @@
               <v-col
                 xl="5" lg="5" md="5" sm="5" xs="12" cols="12" 
               >
-                    <v-card
-
-                    >
-                      <v-img
-                        :src="product.photo"
-                        :lazy-src="product.photo"
-                        aspect-ratio="1"
-                        cover
-                        class="bg-grey-lighten-2"
-                      >
-                        <template v-slot:placeholder>
-                          <v-row
-                            class="fill-height ma-0"
-                            align="center"
-                            justify="center"
+                    
+                    <Carousel>
+                      <Slide v-for="slide in product.photos" :key="slide">
+                        <div class="carousel__item">
+                          <v-img
+                            :src="slide"
+                            :lazy-src="slide"
+                            aspect-ratio="1"
+                            cover
+                            class="bg-grey-lighten-2"
                           >
-                            <v-progress-circular
-                              indeterminate
-                              color="grey-lighten-5"
-                            ></v-progress-circular>
-                          </v-row>
-                        </template>
-                      </v-img>
-                    </v-card>           
+                            <template v-slot:placeholder>
+                              <v-row
+                                class="fill-height ma-0"
+                                align="center"
+                                justify="center"
+                              >
+                                <v-progress-circular
+                                  indeterminate
+                                  color="grey-lighten-5"
+                                ></v-progress-circular>
+                              </v-row>
+                            </template>
+                          </v-img>
+                        </div>
+
+                      </Slide>
+
+                      <template #addons>
+                        <Navigation />
+                        <Pagination />
+                      </template>
+                    </Carousel>                        
+              
               </v-col>
               <v-col
                 xl="7" lg="7" md="7" sm="7" xs="12" cols="12" 
@@ -41,14 +51,6 @@
                   <p>{{ product.description }}</p>
                   <p><b>{{ product.price }}</b></p>
                   <br/>
-                  <!-- 
-                  <v-btn
-                    depressed
-                    color="success"
-                  >
-                    Comprar
-                  </v-btn>                   
-                  -->
                   <button-buy-whats-app 
                     phoneNumber="11961409798" 
                     :message="messageWhatsAPP()" 
@@ -67,15 +69,24 @@
   import AppBar from '../components/AppBar' 
   import ButtonBuyWhatsApp from '../components/ButtonBuyWhatsApp'
   import productGateway from '../gateway/product-gateway.js'
+  
+  import 'vue3-carousel/dist/carousel.css'
+  import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'  
+  
   export default {
     components: {
       AppBar,
-      ButtonBuyWhatsApp 
+      ButtonBuyWhatsApp,
+      Carousel,
+      Slide,
+      Pagination,
+      Navigation,      
     },
     data: () => ({ product: {} }),
     beforeMount() {
       productGateway.getProductById(this.$route.params._id, res => {
           this.product = res
+          this.product.photos.push(this.product.photo)
       })
     },
     methods: {
@@ -85,3 +96,25 @@
     }
   }
 </script>
+<style>
+.carousel__item {
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+/* .carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+} */
+</style>
