bf90a9da1dd1a0b77db9d64d64a393b8b176a4a9
Diego Lirio <ddamacena@uolinc.com>
2023-01-07 15:07:34 -0300

    progess circular component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <v-app>
+  
+  <!-- <v-app>
     <v-main>
       <router-view/>
     </v-main>
+  </v-app> -->
+
+  <v-app id="inspire">         
+    <router-view v-if="!loadingSite" />  
+    <v-progress-circular
+      v-else
+      :size="100"
+      :width="7"
+      color="primary"
+      indeterminate
+      class="centered"
+    ></v-progress-circular>    
   </v-app>
+
 </template>
 
 <script>
@@ -12,7 +26,18 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    loadingSite: false,
   }),
+  beforeMount() {
+    this.loadingSite = false 
+    // TODO get web-site by domain ---> location.host
+  }
 }
 </script>
+<style scoped>
+  .centered {
+    position: fixed; /* or absolute */
+    top: 30%;
+    left: 40%;
+  }
+</style>
